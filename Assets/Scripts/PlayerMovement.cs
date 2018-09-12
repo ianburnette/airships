0150f73e9a03aab0454bf5bdfd5055d02991bb49
Ian Burnette <ian.burnette@eidu.com>
2018-09-12 23:22:58 +0200

    Working on Pseudorandom map based on original map design https://trello.com/c/wGZTfOLZ/32-work-on-map-design

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using ProBuilder2.Common;
 using UnityEngine;
@@ -20,10 +21,12 @@ public class PlayerMovement : MonoBehaviour {
     [SerializeField] float[] distances;
     [SerializeField] float distanceSimilarityMargin;
 
+    public InputEvaluation currentInputEvaluation;
     Vector2 previousFramePosition;
     public Vector2 origin;
 
     public Vector2 mostRecentInput;
+    [SerializeField] int degreesPerInputMutation = 5;
 
     public delegate void Move(float var);
     public static event Move OnMove;
@@ -39,7 +42,11 @@ public class PlayerMovement : MonoBehaviour {
     }
 
 
-    void Start() => previousFramePosition = transform.position;
+    void Start() {
+        previousFramePosition = transform.position;
+        distances = new float[3];
+    }
+
     void Update() {
             FaceInputDirection();
       //  DebugRays();
@@ -56,39 +63,66 @@ public class PlayerMovement : MonoBehaviour {
     float MovementAngle(Vector2 dir) => Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
     void ProcessMovementInput(Vector2 input) {
-        ApplyMovement(SanitizeInput(input.normalized));
+        //DEBUG
+        currentInputEvaluation = EvaluateInput(CastRays(GetRays()));
+        //END DEBUG
+        //TestInput (input.normalized);
+        ApplyMovement(input);
         mostRecentInput = input;
     }
 
-    Vector2 SanitizeInput(Vector2 input) {
-        //
-        while (!InputAcceptable(input))
-//        var distances = CastRays(GetRays(), input);
-
-        ProcessDistances(input, distances);
-        return input;
+    void TestInput(Vector2 currentInput) {
+        distances = CastRays(GetRays());
+        var eval = EvaluateInput(distances);
+        if (eval == InputEvaluation.valid)
+            ApplyMovement(currentInput);
+        else
+            TestInput(MutateInput(currentInput, eval));
     }
 
-    bool InputAcceptable(Vector2 input) {
-        return true;
+    Vector2 MutateInput(Vector2 inputToMutate, InputEvaluation eval) {
+        switch (eval) {
+        case InputEvaluation.facingWall:
+            return Vector2.zero;
+            break;
+        case InputEvaluation.wallToRight:
+            return inputToMutate.Rotate(-degreesPerInputMutation);
+            break;
+        case InputEvaluation.wallToLeft:
+            return inputToMutate.Rotate(degreesPerInputMutation);
+            break;
+        case InputEvaluation.valid:
+            print("trying to mutate valid input");
+            return inputToMutate;
+            break;
+        default:
+            throw new ArgumentOutOfRangeException(nameof(eval), eval, null);
+        }
     }
 
-    float[] CastRays(Ray[] rays, Vector2 input) {
-        for (var i = 0; i < 3; i++) distances[i] = Cast(rays[i]);
+    InputEvaluation EvaluateInput(float[] currentDistances) {
+        if (AllDistancesAreVirtuallyTheSame(currentDistances) &&
+            AllDistancesAreGreaterThanZero(currentDistances))
+            return InputEvaluation.facingWall;
+        if (WallToTheLeft(currentDistances))
+            return InputEvaluation.wallToLeft;
+        if (WallToTheRight(currentDistances))
+            return InputEvaluation.wallToRight;
+        return InputEvaluation.valid;
+    }
+
+    static bool WallToTheRight(float[] currentDistances) => currentDistances[2] > 0f && currentDistances[0] > 0f;
+    static bool WallToTheLeft(float[] currentDistances) => currentDistances[1] > 0f && currentDistances[0] > 0f;
+
+    static bool AllDistancesAreGreaterThanZero(float[] distances) =>
+        (distances[0] > 0 && distances[1] > 0 && distances[2] > 0);
+
+    bool AllDistancesAreVirtuallyTheSame(float[] distances) =>
+        ((distances[0] + distances[1] + distances[2]) / 3) < distanceSimilarityMargin;
+
+    float[] CastRays(Ray[] rays) {
+        for (var i = 0; i < rays.Length; i++) distances[i] = Cast(rays[i]);
         return distances;
-    }
-
-    Vector2 ProcessDistances(Vector2 input, float[] distances) {
-        // 0 = forward, 1 = left, 2 = right
-        //if (distances[1] > 0f && distances [0] > 0f)
-        //    // wall to the left
-        //if (distances[2] > 0f && distances[0] > 0f)
-        //    //wall to the right
-        //if (((distances[0] + distances[1] + distances[2]) / 3) < distanceSimilarityMargin)
-        //    //heading towards wall
-        //    if (distances[0] > 0 && distances[1] > 0 && distances[2] > 0)
-                //facing wall
-        return input;
     }
 
     float Cast(Ray ray) {
@@ -100,6 +134,7 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     Ray[] GetRays() {
+        //TODO this should look at input, not only facing direction (and perhaps not at all)
         var rays = new Ray[3];
         var pos = (Vector2)transform.position;
         var dir = (Vector2)transform.up * movementDirectionRayLength;
@@ -136,4 +171,11 @@ public class PlayerMovement : MonoBehaviour {
         rigidbody2D.velocity = Vector2.zero;
         rigidbody2D.angularVelocity = 0f;
     }
+}
+
+public enum InputEvaluation {
+    facingWall,
+    wallToRight,
+    wallToLeft,
+    valid
 }
