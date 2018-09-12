ec6dc4434f61aad183fd509aa7006135bdd30b9d
Ian Burnette <ian.burnette@eidu.com>
2018-09-12 08:32:07 +0200

    https://trello.com/c/FmtvKVaE/34-make-camera-take-longer-to-begin-looking-in-movement-direction

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -9,21 +9,38 @@ public class CameraFollow : MonoBehaviour {
     [SerializeField] float lookAheadDistMult;
     [SerializeField] Vector2 lookAheadAspectRatio;
     [SerializeField] Vector2 offset;
+    [SerializeField] float lookAheadSmootheTime;
+    Rigidbody2D rb;
+    Vector2 currentLookAheadOffset;
     public Vector2 Offset { get { return offset; } set { offset = value; } }
     public Transform Target { get { return target; } set { target = value; } }
-    void OnEnable() => cam = this;
+    void OnEnable() {
+        cam = this;
+        GetCameraTarget();
+    }
+
+    void GetCameraTarget() {
+        rb = target.GetComponent<Rigidbody2D>();
+    }
 
     void FixedUpdate() =>
-        transform.position = Vector2.Lerp(transform.position, GetPostition(), followSpeed * Time.deltaTime);
+        transform.position = Vector2.Lerp(transform.position, GetPosition(), followSpeed * Time.deltaTime);
 
-    Vector2 GetPostition() {
-        var rb = target.GetComponent<Rigidbody2D>();
+    Vector2 GetPosition() {
         if (rb == null) return target.position;
+        currentLookAheadOffset = SmoothePosition(GetLookAheadPosition());
+        return (Vector2)target.position + currentLookAheadOffset;
+    }
+
+    Vector2 SmoothePosition(Vector2 pos) =>
+        Vector2.Lerp(currentLookAheadOffset, pos, lookAheadSmootheTime * Time.deltaTime);
+
+    Vector2 GetLookAheadPosition() {
         lookAheadAspectRatio = CorrectForAspectRatio(rb.velocity);
-        return (Vector2)target.position + (new Vector2
-                                               (rb.velocity.x * lookAheadAspectRatio.x,
-                                                rb.velocity.y * lookAheadAspectRatio.y)
-                                           + offset);
+        return (new Vector2
+                    (rb.velocity.x * lookAheadAspectRatio.x,
+                     rb.velocity.y * lookAheadAspectRatio.y)
+                + offset);
     }
 
     Vector2 CorrectForAspectRatio(Vector2 vel) =>
